4eb99062ffe0f7102b735475fad17c8415558a8e
Joshua Lee <30053818@tafe.wa.edu.au>
2022-05-19 15:36:16 +0800

    Bug Fixed

---
+++ AT01_UnityProject/Assets/Scripts/Enemy.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Tooltip("Movement speed modifier.")]
+    [SerializeField] private float speed = 3;
+    private Node currentNode;
+    private Vector3 currentDir;
+    private bool playerCaught = false;
+
+    public delegate void GameEndDelegate();
+    public event GameEndDelegate GameOverEvent = delegate { };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeAgent();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerCaught == false)
+        {
+            if (currentNode != null)
+            {
+                //If beyond 0.25 units of the current node.
+                if (Vector3.Distance(transform.position, currentNode.transform.position) > 0.25f)
+                {
+                    transform.Translate(currentDir * speed * Time.deltaTime);
+                }
+                //Implement path finding here
+                else 
+                { 
+                    //find new target node
+                    //if target node is not the AIs current node is not null
+                        //set current node to traget node
+                    //else if player traget not null and player traget node not current node
+                        //set current node to players target node
+
+                    //if current node is not null
+                        //set current direction towards node
+                        //normalize current direction
+                }
+
+            }
+            else
+            {
+                Debug.LogWarning($"{name} - No current node");
+            }
+
+            Debug.DrawRay(transform.position, currentDir, Color.cyan);
+        }
+    }
+
+    //Called when a collider enters this object's trigger collider.
+    //Player or enemy must have rigidbody for this to function correctly.
+    private void OnTriggerEnter(Collider other)
+    {
+        if (playerCaught == false)
+        {
+            if (other.tag == "Player")
+            {
+                playerCaught = true;
+                GameOverEvent.Invoke(); //invoke the game over event
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sets the current node to the first in the Game Managers node list.
+    /// Sets the current movement direction to the direction of the current node.
+    /// </summary>
+    void InitializeAgent()
+    {
+        currentNode = GameManager.Instance.Nodes[0];
+        currentDir = currentNode.transform.position - transform.position;
+        currentDir = currentDir.normalized;
+    }
+
+    //Implement DFS algorithm method here
+    private Node DepthFirstSearch()
+    {
+        Stack nodeStack = new Stack(); //Stacks the unvisited nodes, last one added to stack is next visited
+        List<Node> visitedNodes = new List<Node> (); //track visited nodes
+        nodeStack.Push(GameManager.Instance.Nodes[0]); //add root node to stack 
+
+        while (nodeStack.Count > 0) //while stack is not empty
+        { 
+            Node current = (Node)nodeStack.Pop(); //pop the last node added to stack
+            visitedNodes.Add(currentNode); //mark current node as visited
+            foreach (Node child in currentNode.Children)
+            {
+                if (visitedNodes.Contains(child) == false && nodeStack.Contains(child) == false)
+                {
+                    if (child == GameManager.Instance.Player.CurrentNode) //check if the child is equal to players current node
+                    {
+                        //if so, return the child
+                        return child;
+                    }
+                    nodeStack.Push(child); //push child to node
+                }
+            }
+        }
+        return null; //traget not found
+    }
+}
---
+++ AT01_UnityProject/Assets/Widget.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Widget : MonoBehaviour
+{
+    [SerializeField] private Image[] icons;
+
+    private float flashTimer = -1;
+    private Player player;
+
+    private void Awake()
+    {
+        //DO NOT USE
+        // > FindObjectOfType<Enemy>(); <
+        FindObjectOfType<Enemy>().GameOverEvent += delegate 
+        {
+            //Debug.Log("Disabling UI");
+            gameObject.SetActive(false); //turn wight off when game over
+        };
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameManager.Instance.Player;
+        //subscribe to player input event
+        player.InputEvent += MovementInput;
+        for (int i = 0; i < icons.Length; i++) //setting icons to default colour 
+        { 
+            icons[i].color = Color.grey;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    
+    {
+        if (flashTimer >= 0)
+        { 
+            flashTimer += Time.deltaTime;
+            if (flashTimer > 0.1f)
+            { 
+                flashTimer = -1;
+                for (int i = 0; i < icons.Length; i++)  //setting icons to default colour 
+                {
+                    icons[i].color = Color.grey;
+                }
+            }
+        
+        }
+    }
+    public void MovementInput(char c)
+    { 
+        flashTimer = 0;
+        switch (c)
+        {
+            case 'l':
+                if(player.CurrentNode.Parents.Length > 0)
+                {
+                    foreach (Node node in player.CurrentNode.Parents[0].Children)
+                    {
+                        if (node.transform.position.x == player.CurrentNode.transform.position.x - 10)
+                        { 
+                            player.MoveToNode(node);
+                            icons[0].color = Color.green;
+                            return;
+                        }
+                    }
+                }
+                icons[0].color = Color.red;
+                break;
+            case 'r':
+                if (player.CurrentNode.Parents.Length > 0)
+                {
+                    foreach (Node node in player.CurrentNode.Parents[0].Children)
+                    {
+                        if (node.transform.position.x == player.CurrentNode.transform.position.x + 10)
+                        {
+                            //move player to node
+                            player.MoveToNode(node);
+                            icons[1].color = Color.green;
+                            return;
+                        }
+                    }
+                }
+                icons[1].color= Color.red;
+                break;
+            case 'u':
+                if (player.CurrentNode.Parents.Length > 0 && player.CurrentNode.Parents[0].Parents.Length > 0)
+                {
+                    //move player to parent of current node
+                    player.MoveToNode(player.CurrentNode.Parents[0]);
+                    icons[2].color = Color.green;
+                }
+                else
+                {
+                    icons[2].color = Color.red;
+                }
+                break;
+            case 'd':
+                if (player.CurrentNode.Children.Length > 0)
+                {
+                    //move player to first child
+                    player.MoveToNode(player.CurrentNode.Children[0]);
+                    icons[3].color = Color.green;
+                }
+                else
+                {
+                    icons[3].color = Color.red;
+                }
+                break;
+        }
+    }
+
+
+
+}
